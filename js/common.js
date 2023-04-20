350c8a31eeb6baae4b66c7215643570cd0fe75e7
KSY <tjssud0647@gmail.com>
2023-04-20 14:38:29 +0900

    update

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 $(function () {
+  // index.html
   var tabs = $('.tab-best-sellers ul li');
   var slide = $('.tab-slide-wrap .slide');
 
@@ -9,6 +10,23 @@ $(function () {
         $(this).addClass('on');
         slide.removeClass('display-on');
         slide.eq(i).addClass('display-on');
+      }
+    }
+  });
+
+  // login.html
+  var memberTab = $('.content .member-tab div');
+  var loginForm = $('.content .login-box > div');
+
+  console.log(loginForm);
+
+  memberTab.click(function () {
+    for (let i = 0; i < 2; i++) {
+      if ($(this).index() === i) {
+        memberTab.removeClass('pick');
+        $(this).addClass('pick');
+        loginForm.removeClass('login-box-on');
+        loginForm.eq(i).addClass('login-box-on');
       }
     }
   });
