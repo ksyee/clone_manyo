fcfb249997419eef0848702f9748e7c16c885f3d
KSY <tjssud0647@gmail.com>
2023-04-21 10:52:15 +0900

    add swiper js

--- js/common.js
+++ js/common.js
@@ -1,5 +1,15 @@
 $(function () {
-  // index.html
+  //header scroll event
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+      $('#header-flex').addClass('on');
+    } else {
+      $('#header-flex').removeClass('on');
+    }
+  }); // $(window).scroll
+
+  /* index.html */
   var tabs = $('.tab-best-sellers ul li');
   var slide = $('.tab-slide-wrap .slide');
 
@@ -12,9 +22,14 @@ $(function () {
         slide.eq(i).addClass('display-on');
       }
     }
+  }); // tabs.click
+
+  var swiper = new Swiper('.slide-wrap', {
+    slidesPerView: 1,
+    loop: true,
   });
 
-  // login.html
+  /* login.html */
   var memberTab = $('.content .member-tab div');
   var loginForm = $('.content .login-box > div');
 
@@ -27,18 +42,24 @@ $(function () {
         loginForm.eq(i).addClass('login-box-on');
       }
     }
-  });
+  }); // memberTab.click
 
-  // customer service
-  var questionTab = $('.board-table tr.question');
-  var answerTab = $('.board-table tr.answer');
+  /* customer_service.html */
+  var questionTab = $('.board-table div.question div p');
+  var answerTab = $('.board-table div.answer');
+  var answerHeight = $('.board-table div.answer div').outerHeight();
 
   questionTab.click(function () {
     console.log($(questionTab).index(this));
     for (let i = 0; i < questionTab.length; i++) {
       if ($(questionTab).index(this) === i) {
         answerTab.eq(i).toggleClass('answer-on');
+        if (answerTab.eq(i).hasClass('answer-on')) {
+          answerTab.eq(i).css('height', answerHeight);
+        } else {
+          answerTab.eq(i).css('height', 0);
+        }
       }
     }
-  });
+  }); // questionTab.click
 });
